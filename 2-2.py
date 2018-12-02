4d82c6ad8521b7bf07633b2cbe6ea423228cf251
Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
2018-12-02 19:01:10 +0100

    Add second day part two

---
+++ 2-2.py
@@ -0,0 +1,12 @@
+with open("2.input") as f:
+    data = f.read().splitlines()
+
+l = len(data)
+for i in range(l):
+    i_data = data[i]
+    for j in range(i, l):
+        j_data = data[j]
+        diff = sum(1 for a, b in zip(i_data, j_data) if a != b)
+        if diff == 1:
+            print("".join(a for a,b in zip(i_data, j_data) if a==b))
+            break
